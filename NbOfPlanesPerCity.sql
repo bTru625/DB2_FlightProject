7475978650f7a5b8dae0a4529921b7c474236670
Brett Trudel <trudel25@gmail.com>
2025-11-05 19:04:16 -0500

    Create NbOfPlanesPerCity.sql

---
+++ NbOfPlanesPerCity.sql
@@ -0,0 +1,4 @@
+/*
+a) Create the stored function NbOfPlanesPerCity that accepts the parameter : city name and returns the number of planes located in that city.
+b) Test the function NbOfPlanesPerCity (the city name could be entered in upper or lower case)
+*/
