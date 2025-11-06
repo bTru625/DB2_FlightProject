980f171c6cdf2888d70d7d2ae1bf965c20fac4a1
Brett Trudel <trudel25@gmail.com>
2025-11-05 19:05:03 -0500

    Create ListOfFlights_ByCity.sql

---
+++ ListOfFlights_ByCity.sql
@@ -0,0 +1,4 @@
+/*
+a) Create the stored procedure ListOfFlights that accepts the parameter : city name (departure city) and displays the list of flights ordered in ascending order of departure time (the columns to display are : flight id, pilot name, plane description, departure time, arrival time, arrival city name)
+b) Test the procedure ListOfFlights (the city name could be entered in upper or lower case)
+*/
