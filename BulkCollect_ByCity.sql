48973892fffdfd83ad7c501e30aa066dcca124a5
Brett Trudel <trudel25@gmail.com>
2025-11-05 19:00:54 -0500

    Create BulkCollect_ByCity.sql

---
+++ BulkCollect_ByCity.sql
@@ -0,0 +1,4 @@
+/*
+a) By using SELECT .. BULK COLLECT .., type a plsql program that asks to enter a city id and display all planes based in this city (pla_id, desc, max_passenger and city name)
+b) Execute this code for the cities : 102, 101
+*/
