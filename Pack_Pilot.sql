1c2a36b256320423cffbb20d91fd08635e0b082c
Brett Trudel <trudel25@gmail.com>
2025-11-05 19:05:50 -0500

    Create Pack_Pilot.sql

---
+++ Pack_Pilot.sql
@@ -0,0 +1,8 @@
+/*
+a) Create the package and package body called Pack_Pilot that defines the following objects :
+- Update_Salary : allows increase/decrease the salary of pilot according to a new amount or a percentage (use a technic to define an overload object with the same function/procedure name) (the info to output : pilot id, last name, old salary, new salary, the amount or percentage)
+- List_Of_Pilots : pilots who pilot a particular plane (pilot_id, last_name, pla_desc pilot city_name) (use cursor with parameter)
+- Nb_Planes : Returns the total number of planes flown by a given pilot.
+b) Type a plsql program that test the objects of this package
+- Test Update_Salary, List_Of_Pilots and Nb_Planes
+*/
