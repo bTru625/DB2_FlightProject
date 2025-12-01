1595837dc2a7826229576e469cf4d2c2b3ad0e82
Brett Trudel <trudel25@gmail.com>
2025-11-30 21:30:23 -0500

    Completed Q6_Spec

--- Q6_Spec.sql
+++ Q6_Spec.sql
@@ -1,1 +1,23 @@
 
+
+CREATE or REPLACE PACKAGE Pack_Pilot IS
+
+Procedure Update_Salary (
+    p_pilot_id in pilot.pilot_id%type,
+    p_amount in pilot.salary%type
+) ;
+
+Procedure Update_Salary (
+    p_pilot_id in pilot.pilot_id%type,
+    p_percent in number
+) ;
+
+Procedure List_of_pilots (p_pla_id plane.pla_id%TYPE);
+
+FUNCTION Nb_Planes (p_pilot_id in pilot.pilot_id%type)
+    RETURN NUMBER;
+
+
+END Pack_Pilot;
+/
+
