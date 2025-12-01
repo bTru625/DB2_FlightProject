c4ec84d452dd52f393a6cba0c0eb430a151c7dcb
shayan <shayan.delbari@protonmail.com>
2025-11-30 19:17:14 -0500

    Q4 done

--- Q4.sql
+++ Q4.sql
@@ -2,3 +2,18 @@
 a) Create the stored function NbOfPlanesPerCity that accepts the parameter : city name and returns the number of planes located in that city.
 b) Test the function NbOfPlanesPerCity (the city name could be entered in upper or lower case)
 */
+CLEAR SCREEN
+
+CREATE OR REPLACE FUNCTION NbOfPlanesPerCity (V_CityName CITY.CITY_NAME%type)
+RETURN NUMBER
+IS
+    V_NumberOfPlanes NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO V_NumberOfPlanes
+    FROM PLANE p
+    JOIN CITY  c ON p.CITY_ID = c.CITY_ID
+    WHERE UPPER(c.CITY_NAME) = UPPER(V_CityName);
+
+    RETURN V_NumberOfPlanes;
+END;
+/
--- Q4_Test.sql
+++ Q4_Test.sql
@@ -1,1 +1,20 @@
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+CLEAR SCREEN
 
+ACCEPT S_CityName PROMPT 'Please enter a city name: '
+
+DECLARE
+    V_CityName CITY.CITY_NAME%TYPE := '&S_CityName';
+    V_Result NUMBER;
+
+BEGIN
+    V_Result := NbOfPlanesPerCity(V_CityName);
+    DBMS_OUTPUT.PUT_LINE('The number of planes in that city is '|| V_Result);
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE(V_CityName || 'was not in the database. ');
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE(SQLERRM);
+END;
+/
