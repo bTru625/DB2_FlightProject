aee0518098c67da9e251c92907ffc19c234e322f
Brett Trudel <trudel25@gmail.com>
2025-11-30 19:16:47 -0500

    Progress on Q6 (update Salary)

--- Q6.sql
+++ Q6.sql
@@ -6,3 +6,96 @@ a) Create the package and package body called Pack_Pilot that defines the follow
 b) Type a plsql program that test the objects of this package
 - Test Update_Salary, List_Of_Pilots and Nb_Planes
 */
+
+
+CREATE or REPLACE PACKAGE BODY Pack_Pilot IS
+
+---- PROCEDURE: UPDATE SALARY (TWO OVERLOADS)
+
+-- 1) has pilot id & new salary
+Procedure Update_Salary (
+    p_pilot_id in pilot.pilot_id%type,
+    p_amount in pilot.salary%type
+) IS
+
+    V_OLD_SALARY pilot.salary%Type;
+    V_NEW_SALARY pilot.salary%Type;
+    V_LAST_NAME pilot.LAST_NAME%TYPE;
+
+
+BEGIN
+
+    SELECT salary, last_name
+    into V_OLD_SALARY, V_LAST_NAME
+    FROM PILOT
+    WHERE PILOT_ID = P_PILOT_ID;
+
+    V_NEW_SALARY := V_OLD_SALARY + p_amount;
+
+    --CHECK THAT SALARAY IS NOT NEG
+    IF V_NEW_SALARY < 0 THEN
+        V_NEW_SALARY := 0;
+        DBMS_OUTPUT.PUT_LINE('Salary cannot be negative, salary has been set to $0');
+    END IF;
+
+    UPDATE PILOT
+    SET salary = V_NEW_SALARY
+    WHERE PILOT_ID = p_pilot_id;
+
+    DBMS_OUTPUT.PUT_LINE('SALARY UPDATED (by amount');
+    DBMS_OUTPUT.PUT_LINE('______________________________');
+    DBMS_OUTPUT.PUT_LINE('Pilot ID:     ' || p_pilot_ID);
+    DBMS_OUTPUT.PUT_LINE('Pilot Name:   ' || V_LAST_NAME);
+    DBMS_OUTPUT.PUT_LINE('Old Salary:   ' || V_OLD_SALARY);
+    DBMS_OUTPUT.PUT_LINE('Salary Change: $' || p_amount);
+    DBMS_OUTPUT.PUT_LINE('New Salary:   ' || V_NEW_SALARY);
+
+END Update_Salary;
+
+
+
+
+-- 2) has pilot ID and change in salary (as %)
+Procedure Update_Salary (
+    p_pilot_id in pilot.pilot_id%type,
+    p_amount in pilot.salary%type
+) IS
+
+    V_OLD_SALARY pilot.salary%Type;
+    V_NEW_SALARY pilot.salary%Type;
+    V_LAST_NAME pilot.LAST_NAME%TYPE;
+
+
+BEGIN
+
+    SELECT salary, last_name
+    into V_OLD_SALARY, V_LAST_NAME
+    FROM PILOT
+    WHERE PILOT_ID = P_PILOT_ID;
+
+    V_NEW_SALARY := V_OLD_SALARY + p_amount;
+
+    --CHECK THAT SALARAY IS NOT NEG
+    IF V_NEW_SALARY < 0 THEN
+        V_NEW_SALARY := 0;
+        DBMS_OUTPUT.PUT_LINE('Salary cannot be negative, salary has been set to $0');
+    END IF;
+
+    UPDATE PILOT
+    SET salary = V_NEW_SALARY
+    WHERE PILOT_ID = p_pilot_id;
+
+    DBMS_OUTPUT.PUT_LINE('SALARY UPDATED (by amount');
+    DBMS_OUTPUT.PUT_LINE('______________________________');
+    DBMS_OUTPUT.PUT_LINE('Pilot ID:     ' || p_pilot_ID);
+    DBMS_OUTPUT.PUT_LINE('Pilot Name:   ' || V_LAST_NAME);
+    DBMS_OUTPUT.PUT_LINE('Old Salary:   ' || V_OLD_SALARY);
+    DBMS_OUTPUT.PUT_LINE('Salary Change: $' || p_amount);
+    DBMS_OUTPUT.PUT_LINE('New Salary:   ' || V_NEW_SALARY);
+
+END Update_Salary;
+
+
+
+
+
